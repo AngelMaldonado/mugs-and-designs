f1f57d48a96e6a11b7964b4d306fe804d461d850
AngelMaldonado <amaldonadojuarez@gmail.com>
2025-03-22 16:01:23 -0600

    fix: workerMode option

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -9,6 +9,8 @@ module.exports = defineConfig({
   },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server" || "shared",
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -16,8 +18,6 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
-    redisUrl: process.env.REDIS_URL,
   },
   modules: [
     {
